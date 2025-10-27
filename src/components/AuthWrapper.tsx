1b739043684bbe7e7a2ce116ab37fe59e412accc
Deep Bhanushali <bhanushalideep24@gmail.com>
2025-10-27 13:53:00 +0530

    final commit diwali ke baad

--- src/components/AuthWrapper.tsx
+++ src/components/AuthWrapper.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import { AuthProvider } from '@/lib/AuthContext';
-import SocketManager from './SocketManager';
 
 export function AuthWrapper({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <SocketManager>
-        {children}
-      </SocketManager>
+      {children}
     </AuthProvider>
   );
 }
